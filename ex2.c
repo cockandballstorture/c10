d4ca177d01280d022c3193ed6b49b1acef24a9ed
cockandballstorture <72618474+cockandballstorture@users.noreply.github.com>
2021-02-23 16:48:27 +0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -2,43 +2,57 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-int one();
-int two();
-int fri();
+int* one();
+int** two();
+int*** fri();
 int main(){
 	FILE *f;
-	int a;
+	int a,i,j;
 	int b;
 	int c;
-	int w[];
+	int *p;
+	int **q;
+	int ***k;
 	f=fopen("C:cr.txt","r+b");
 	fread(&a, sizeof(int*),1,f);
-	scanf("%d",&a);
 	switch(a){
 	case 1:
 		fread(&a, sizeof(int*),1,f);
-	    w=one(a);
+	    p=one(a);
+	    free(p);
 	    break;
 	case 2:
 		fread(&a, sizeof(int*),1,f);
 		fread(&b, sizeof(int*),1,f);
-	    w=two(a,b);
+	    q=two(a,b);
+	    for (i = 0; i < a; i++) {
+	            free(p[i]);
+	        }
+	    free(p);
 	    break;
 	case 3:
 		fread(&a, sizeof(int*),1,f);
 		fread(&b, sizeof(int*),1,f);
 		fread(&c, sizeof(int*),1,f);
-		w=fri(a,b,c);
+		k=fri(a,b,c);
+		for (i = 0; i < b; i++) {
+		        for (i = 0; i < c; i++) {
+		                free(p[i][j]);
+		        }
+		        free(p[i]);
+		    }
+		free(p);
 		break;
 	default:
 	 printf("no");
 	}
 }
-int one(int a){
+int* one(int a){
 	int i;
 	FILE *f;
 	int p;
-	int w[a];
+	int *q;
+	q=(int*)malloc(a*sizeof(int));
 	fpos_t *s;
 	int e=1;
 	s=&e;
@@ -46,45 +60,50 @@ int one(int a){
 	fsetpos(f,s);
 	for (i=0;i<a;i++){
 		fread(&p, sizeof(int*),1,f);
-		w[i];
+		q[i]=p;
 	}
-	return w;
+	return(q);
 }
-int two(int a,int b){
+int** two(int a,int b){
 	int i,j;
 		FILE *f;
 		int p;
-		int w[a][b];
+		int **q;
 		fpos_t *s;
 		int t=2;
 		s=&t;
 		f=fopen("C:cr.txt","w+b");
 		fsetpos(f,s);
+		q=(int**)malloc(a*sizeof(int*));
 		for (i=0;i<a;i++){
+			q[i] = (int*) malloc(b * sizeof(int));
 			for (j=0;j<b;j++){
 			fread(&p, sizeof(int*),1,f);
-			w[i][j]=p;
+			q[i][j]=p;
 			}
 }
-		return w;
+		return (q);
 }
-int fri(int a,int b,int c){
+int*** fri(int a,int b,int c){
 	int i,j,k;
 			FILE *f;
 			int p;
-			int w[a][b][c];
+			int ***q;
 			fpos_t *s;
 			int t=3;
 			s=&t;
 			f=fopen("C:cr.txt","w+b");
 			fsetpos(f,s);
+			q=(int***)malloc(a*sizeof(int*));
 			for (i=0;i<a;i++){
+				q[i]=(int**)malloc(b*sizeof(int*));
 				for (j=0;j<b;j++){
+					q[i][j]=(int*)malloc(c*sizeof(int));
 					for (k=0;k<c;k++){
 						fread(&p, sizeof(int*),1,f);
-						w[i][j][k]=p;
+						q[i][j][k]=p;
 					}
 				}
 			}
-			return w;
+			return (q);
 }
